59abd0f12a431fea6555ae93f74fe09f3a106739
CRIS4566 <114404431+CRIS4566@users.noreply.github.com>
2022-11-20 16:54:05 +0900

    wetube study

---
+++ src/init.js
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import "./db";
+import "./models/Video";
+import "./models/User";
+import app from "./server";
+
+const PORT = 4000;
+
+const handleListening = () => console.log("✅Server listenting on port http://localhost:4000 🚀");
+
+app.listen(PORT, handleListening);
